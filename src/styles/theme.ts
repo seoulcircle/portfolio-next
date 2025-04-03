9d4aa19303b419a348f6f930c7c4e3e47cecd362
seoulcircle <dlghd1997@naver.com>
2025-04-04 00:53:02 +0900

    배포 오류 fix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,26 +1,26 @@
 // src/styles/theme.ts
 import "@emotion/react";
 
-export const theme = {
-  pointBlue: "#0064FF",
-  pointGrey: "#202632",
-  black: {
-    veryDark: "#141414",
-    darker: "#181818",
-    lighter: "#2F2F2F",
-  },
-  white: {
-    lighter: "#fff",
-    darker: "#e5e5e5",
-  },
-} as const;
+// export const theme = {
+//   pointBlue: "#0064FF",
+//   pointGrey: "#202632",
+//   black: {
+//     veryDark: "#141414",
+//     darker: "#181818",
+//     lighter: "#2F2F2F",
+//   },
+//   white: {
+//     lighter: "#fff",
+//     darker: "#e5e5e5",
+//   },
+// } as const;
 
-export type ThemeType = typeof theme;
+// export type ThemeType = typeof theme;
 
-declare module "@emotion/react" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends ThemeType {}
-}
+// declare module "@emotion/react" {
+//   // eslint-disable-next-line @typescript-eslint/no-empty-interface
+//   export interface Theme extends ThemeType {}
+// }
 
 export const breakpoints = {
   mobile: "639px",
