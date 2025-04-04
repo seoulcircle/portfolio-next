c752a2c2e3e989f0fe4348419137a2451814e586
seoulcircle <dlghd1997@naver.com>
2025-04-04 11:17:19 +0900

    pages 폴더 삭제

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { Global } from "@emotion/react";
+import { CacheProvider, Global } from "@emotion/react";
+import createEmotionCache from "@/emotion/createEmotionCache";
 import { globalStyles } from "@/styles/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
+const emotionCache = createEmotionCache();
 const queryClient = new QueryClient();
-export default function ClientProviders({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+
+export default function ClientProviders({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Global styles={globalStyles} />
-      {children}
-    </QueryClientProvider>
+    <CacheProvider value={emotionCache}>
+      <QueryClientProvider client={queryClient}>
+        <Global styles={globalStyles} />
+        {children}
+      </QueryClientProvider>
+    </CacheProvider>
   );
 }
