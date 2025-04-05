cb9b2bfb8b4db168aff98e7dbb380152f213d421
seoulcircle <dlghd1997@naver.com>
2025-04-06 00:03:43 +0900

    spinner 모바일 대응

--- src/features/daypalette/components/Spinner.tsx
+++ src/features/daypalette/components/Spinner.tsx
@@ -1,30 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import styled from "@emotion/styled";
-import isPropValid from "@emotion/is-prop-valid";
-
-const Wrapper = styled.div`
-  width: 80px;
-  height: 80px;
-  position: relative;
-  margin: 0 auto;
-`;
-
-const Bar = styled(motion.div, {
-  shouldForwardProp: (prop) => isPropValid(prop) && prop !== "active",
-})<{ active: boolean }>`
-  position: absolute;
-  width: 6px;
-  height: ${({ active }) => (active ? "24px" : "12px")};
-  background-color: ${({ active }) => (active ? "#202020" : "#ffffff")};
-  border-radius: 3px;
-  top: 50%;
-  left: 50%;
-  transform-origin: center -30px;
-  transition: all 0.2s ease-in-out;
-`;
+import { S } from "@/features/daypalette/styles/Spinner.style";
 
 const Spinner = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -38,9 +15,9 @@ const Spinner = () => {
   }, []);
 
   return (
-    <Wrapper>
+    <S.Wrapper>
       {Array.from({ length: total }).map((_, i) => (
-        <Bar
+        <S.Bar
           key={i}
           active={i === activeIndex}
           style={{
@@ -48,7 +25,7 @@ const Spinner = () => {
           }}
         />
       ))}
-    </Wrapper>
+    </S.Wrapper>
   );
 };
 
