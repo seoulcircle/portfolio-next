c66196ba0d2894f22513027c027c051935b30a8f
seoulcircle <dlghd1997@naver.com>
2025-04-20 21:55:25 +0900

    경로수정 4

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -13,12 +13,12 @@ const nextConfig = {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "@components": path.resolve(__dirname, "../../../packages/components"),
-      "@hooks": path.resolve(__dirname, "../../../packages/hooks"),
-      "@ui": path.resolve(__dirname, "../../../packages/ui"),
-      "@styles": path.resolve(__dirname, "../../../packages/styles"),
-      "@theme": path.resolve(__dirname, "../../../packages/theme"),
-      "@animations": path.resolve(__dirname, "../../../packages/animations"),
+      "@components": path.resolve(__dirname, "../../packages/components"),
+      "@hooks": path.resolve(__dirname, "../../packages/hooks"),
+      "@ui": path.resolve(__dirname, "../../packages/ui"),
+      "@styles": path.resolve(__dirname, "../../packages/styles"),
+      "@theme": path.resolve(__dirname, "../../packages/theme"),
+      "@animations": path.resolve(__dirname, "../../packages/animations"),
       "@assets": path.resolve(__dirname, "src/assets"),
       "@": path.resolve(__dirname), // apps/portfolio
     };
