400d01abaa79cd8ea4c4b72778985fbab9fda8f9
seoulcircle <dlghd1997@naver.com>
2025-04-20 22:53:37 +0900

    롤백

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -3,9 +3,7 @@ const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 const path = require("path");
 /* eslint-enable @typescript-eslint/no-require-imports */
 
-const isCI = process.env.CI === "true";
-const pathDepth = isCI ? "../../../" : "../../";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
@@ -15,21 +13,21 @@ const nextConfig = {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "@components": path.resolve(__dirname, `${pathDepth}packages/components`),
-      "@hooks": path.resolve(__dirname, `${pathDepth}packages/hooks`),
-      "@ui": path.resolve(__dirname, `${pathDepth}packages/ui`),
-      "@styles": path.resolve(__dirname, `${pathDepth}packages/styles`),
-      "@theme": path.resolve(__dirname, `${pathDepth}packages/theme`),
-      "@animations": path.resolve(__dirname, `${pathDepth}packages/animations`),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@": path.resolve(__dirname),
+      "@components": path.resolve(__dirname, "../../packages/components"),
+      "@hooks": path.resolve(__dirname, "../../packages/hooks"),
+      "@ui": path.resolve(__dirname, "../../packages/ui"),
+      "@styles": path.resolve(__dirname, "../../packages/styles"),
+      "@theme": path.resolve(__dirname, "../../packages/theme"),
+      "@animations": path.resolve(__dirname, "../../packages/animations"),
+      "@": path.resolve(__dirname), // apps/portfolio
     };
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "./tsconfig.base.json"),
+        configFile: path.resolve(__dirname, "../../tsconfig.base.json"),
       }),
     ];
+
     return config;
   },
 };
